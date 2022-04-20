48ecb88440ac95d6c9fcc7f0e8990d437751c531
Yishay-Schlesinger <yishlesi@gmail.com>
2022-04-20 18:51:28 +0300

    Fix errors

--- ex1/main.py
+++ ex1/main.py
@@ -1,5 +1,7 @@
 import random
 from argparse import ArgumentParser
+
+import numpy as np
 import pygame as pyg
 
 # initializes
@@ -31,7 +33,7 @@ BOARD_SIZE = [200, 200]
 
 
 class Creature:
-    def __init__(self, location, status, speed):
+    def __init__(self, location, status, speed=1):
         self.status = status
         self.speed = speed
         self.location = location
@@ -42,7 +44,8 @@ class Creature:
 
     def next_gen_move(self, location, status):
         self.status = status
-        self.next_gen_location = location.next_gen_checkin()
+        self.next_gen_location = location
+        self.next_gen_location.next_gen_checkin(self)
 
     def next_gen_update(self):
         self.location = self.next_gen_location
@@ -69,7 +72,10 @@ class Location:
         stat_color_map = {"healthy": (255, 255, 255),
                           "infected": (255, 0, 0),
                           "recovered": (0, 255, 0)}
-        return (0, 0, 0) if self.contain is None else stat_color_map[self.contain.status]
+        if self.contain is None:
+            return (0, 0, 0)
+        else:
+            return stat_color_map[self.contain.status]
 
 
 class World:
@@ -77,9 +83,11 @@ class World:
         self.board_size = board_size
         self.board_rows = board_size[0]
         self.board_cols = board_size[1]
-        self.board = [[Location(i, j) for j in range(len(self.board_rows))] for i in range(len(self.board_cols))]
+        self.board = [[Location(i, j) for j in range(self.board_rows)] for i in range(self.board_cols)]
+        self.infected_num = int(N * D)
         self.creatures = self.initialize_creatures()
         self.locations_to_update = set([])
+        self.P = PA if D < T else PB
 
     def initialize_creatures(self):
         creatures = []
@@ -90,22 +98,21 @@ class World:
         random.shuffle(fast_lst)
 
         # set random list for the infected cells
-        infected_num = int(N * D)
-        infected_lst = ["infected" for inf in range(infected_num)] + ["healthy" for heal in range(N - infected_num)]
+        infected_lst = ["infected" for inf in range(self.infected_num)] + ["healthy" for heal in
+                                                                           range(N - self.infected_num)]
         random.shuffle(infected_lst)
 
         # set random indexes for the creatures
-        rows_idx_lst = [i for i in range(self.board_size[0])]
+        rows_idx_lst = [i for i in range(self.board_rows)]
         random.shuffle(rows_idx_lst)
-        cols_idx_lst = [j for j in range(self.board_size[1])]
+        cols_idx_lst = [j for j in range(self.board_cols)]
         random.shuffle(cols_idx_lst)
-
 
         # create the creatures
         for creature_args in zip(rows_idx_lst[:N], cols_idx_lst[:N], infected_lst, fast_lst):
             c = Creature(self.get_location(creature_args[0], creature_args[1]), creature_args[2], creature_args[3])
             creatures.append(c)
-
+        # return
         return creatures
 
     def get_location(self, i, j):
@@ -116,35 +123,40 @@ class World:
         for c in self.creatures:
             self.creature_next_gen(c)
         self.update_all_locations()
+        self.locations_to_update = set([])
+        for c in self.creatures:
+            c.next_gen_update()
+        self.P = PA if self.infected_num / N < T else PB
 
     def update_all_locations(self):
         for loc in self.locations_to_update:
             loc.next_gen_update()
 
     def creature_next_gen(self, creature):
-        if creature.next_gen_location is not None:
-            vert_mon_opt, horz_mov_opt = self.movement_options(creature)
+        if creature.next_gen_location is None:
+            next_status = ""
             while True:
                 creature_row_idx = creature.location.row_idx
                 creature_col_idx = creature.location.col_idx
                 for step in range(creature.speed):
-                    vert_mov = random.choice(vert_mon_opt)
+                    vert_mov_opt, horz_mov_opt = self.movement_options(creature_row_idx, creature_col_idx)
+                    if next_status=="":
+                        next_status = self.creature_next_status(creature, horz_mov_opt, vert_mov_opt)
+                    vert_mov = random.choice(vert_mov_opt)
                     horz_mov = random.choice(horz_mov_opt)
                     creature_row_idx += vert_mov
                     creature_col_idx += horz_mov
                     next_loc = self.get_location(creature_row_idx, creature_col_idx)
-                if next_loc.contain.next_gen_loc is None:
-                    self.creature_next_gen(next_loc.contain)
+                if next_loc.contain is not None:
+                    if next_loc.contain.next_gen_location is None:
+                        self.creature_next_gen(next_loc.contain)
                 if next_loc.next_gen_contain is None:
-                    creature.next_gen_move(next_loc, self.creature_next_status(creature, horz_mov_opt, vert_mon_opt))
+                    creature.next_gen_move(next_loc, next_status)
                     self.locations_to_update.add(creature.location)
                     self.locations_to_update.add(creature.next_gen_location)
                     return
 
-    def creature_next_status(self, creature, horz_mov_opt, vert_mon_opt):
-        {"healthy": (255, 255, 255),
-         "infected": (255, 0, 0),
-         "recovered": (0, 255, 0)}
+    def creature_next_status(self, creature, horz_mov_opt, vert_mov_opt):
         if creature.status == "recovered":
             return "recovered"
         if creature.status == "infected":
@@ -152,27 +164,43 @@ class World:
             if creature.gen_for_healing == 0:
                 return "infected"
             else:
+                self.infected_num -= 1
                 return "recovered"
 
         infected_around = 0
-        if
+        for y in vert_mov_opt:
+            for x in horz_mov_opt:
+                if self.get_location(creature.location.row_idx + y, creature.location.col_idx + x):
+                    infected_around += 1
+        probability_staying_healthy = pow(self.P, infected_around)
 
+        if np.random.binomial(1, probability_staying_healthy) == 0:
+            creature.gen_for_healing = X
+            self.infected_num += 1
+            return "infected"
+        else:
+            return "healthy"
 
-    def movement_options(self, creature):
+    def movement_options(self, row_idx, col_idx):
         # vertical movement options
         vertical_movement_options = [-1, 0, 1]
-        if creature.location.row_idx == 0:
+        if row_idx == 0:
             vertical_movement_options.remove(-1)
-        elif creature.location.row_idx == self.board_rows - 1:
+        elif row_idx == self.board_rows - 1:
             vertical_movement_options.remove(1)
 
         # horizontal movement options
         horizontal_movement_options = [-1, 0, 1]
-        if creature.location.col_idx == 0:
+        if col_idx == 0:
             horizontal_movement_options.remove(-1)
-        elif creature.location.col_idx == self.board_cols - 1:
+        elif col_idx == self.board_cols - 1:
             horizontal_movement_options.remove(1)
 
         # return
         return vertical_movement_options, horizontal_movement_options
 
+
+world = World()
+world.next_gen()
+world.next_gen()
+world.next_gen()
